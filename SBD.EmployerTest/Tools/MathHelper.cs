d574ef5fe591cdbe4cb7f02ba32fb7fa8d4d7040
Eliseev Eugene <eliseevmail@ya.ru>
2021-02-06 14:17:57 +0300

    Добавьте файлы проекта.

---
+++ SBD.EmployerTest/Tools/MathHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SBD.EmployerTest.Tools
+{
+    internal static class MathHelper
+    {
+        public static bool IsPowerOfThree(int n)
+        {
+            return 1162261467 % n == 0;
+        }
+    }
+}
